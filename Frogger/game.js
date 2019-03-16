251a7a0a49fa42051c0a568a9d0dcd2b57e29767
Yiiiteng <yitwang@ucm.es>
2019-03-16 12:56:40 +0100

    function trunk added

--- Frogger/game.js
+++ Frogger/game.js
@@ -20,11 +20,15 @@ var playGame = function() {
   var board = new GameBoard();
   board.add(new Frog());
 
-  board.add(new Car(cars.car_yellow.sprite, cars.car_yellow.speed));
-  board.add(new Car(cars.car_brown.sprite, cars.car_brown.speed));
-  board.add(new Car(cars.car_blue.sprite, cars.car_blue.speed));
-  board.add(new Car(cars.car_green.sprite, cars.car_green.speed));
-  board.add(new Car(cars.car_red.sprite, cars.car_red.speed));
+  board.add(new Car(objects.car_yellow.sprite, objects.car_yellow.speed));
+  board.add(new Car(objects.car_brown.sprite, objects.car_brown.speed));
+  board.add(new Car(objects.car_blue.sprite, objects.car_blue.speed));
+  board.add(new Car(objects.car_green.sprite, objects.car_green.speed));
+  board.add(new Car(objects.car_red.sprite, objects.car_red.speed));
+ 
+  board.add(new Trunk(objects.trunk2.sprite, objects.trunk2.speed));
+  board.add(new Trunk(objects.trunk3.sprite, objects.trunk3.speed));
+  board.add(new Trunk(objects.trunk1.sprite, objects.trunk1.speed));
  //board.add(new Background());
   //board.add(new Level(level1,winGame));
   Game.setBoard(1,board);
