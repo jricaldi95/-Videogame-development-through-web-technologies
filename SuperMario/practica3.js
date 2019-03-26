eba4e6204076483be9556b8a276583dee919c734
jricaldi95 <jricaldi@ucm.es>
2019-03-26 15:11:37 +0100

    Mario's moves implemented and princess aded

--- SuperMario/practica3.js
+++ SuperMario/practica3.js
@@ -65,6 +65,67 @@ window.addEventListener("load", function() {
 
     });
 
+    Q.scene("endGame", function(stage) {
+        var container = stage.insert(new Q.UI.Container({
+            x: Q.width / 2,
+            y: Q.height / 2,
+            fill: "rgba(0,0,0,0.5)"
+        }));
+
+        var button = container.insert(new Q.UI.Button({
+            x: 0,
+            y: 0,
+            fill: "#CCCCCC",
+            label: "Play Again"
+        }));
+
+        var label = container.insert(new Q.UI.Text({
+            x: 10,
+            y: -10 - button.p.h,
+            label: stage.options.label
+        }));
+
+        button.on("click", function() {
+            StartLevel1();
+        });
+
+        Q.input.on("confirm", this, function() {
+            StartLevel1();
+        });
+
+        container.fit(20);
+
+    });
+
+
+    Q.scene("winGame", function(stage) {
+
+        var container = stage.insert(new Q.UI.Container({
+            x: Q.width / 2,
+            y: Q.height / 2,
+            fill: "rgba(0,0,0,0.5)"
+        }));
+
+        var button = container.insert(new Q.UI.Button({
+            x: 0,
+            y: 0,
+            fill: "#CCCCCC",
+            label: "Play Again"
+        }));
+
+        var label = container.insert(new Q.UI.Text({
+            x: 10,
+            y: -10 - button.p.h,
+            label: stage.options.label
+        }));
+
+        button.on("click", function() {
+            StartLevel1();
+        });
+
+        container.fit(20);
+    });
+
 
     Q.scene("level1", function(stage) {
         Q.stageTMX("level.tmx", stage);
@@ -72,9 +133,13 @@ window.addEventListener("load", function() {
 
         stage.add("viewport").follow(mario);
         stage.viewport.offsetX = -120;
-        stage.viewport.offsetY = 160;
+        stage.viewport.offsetY = 152;
+
+        stage.insert(new Q.Princess({
+            x: 1990,
+            y: 380
+        }));
         
-       // stage.insert(new Goomba(x = 400, y = 380));
     });
 
 
@@ -82,28 +147,86 @@ window.addEventListener("load", function() {
 
     //MARIO
 
-    Q.Sprite.extend("Mario",{
-
+    Q.Sprite.extend("Mario", {
         init: function(p) {
-       
             this._super(p, {
-                sheet: "marioR", 
+                sheet: "marioR",
                 frame: 0,
-                x: 150, 
-                y: 380
+                x: 150,
+                y: 380,
             });
 
-            this.add('2d, platformerControls');
-          
-            console.log(this.p.y);
-            if (this.p.y > 700) {
+            this.add("2d, platformerControls");
 
-                this.destroy();
-               
-                RestartLevel1();
+        },
+        step: function(dt) {
+            if (this.p.vx > 0) { // derecha
+                if (this.p.landed > 0) {
+                    this.p.sheet = "marioR";
+                } 
+                else {
+                    this.p.sheet = "marioJumpR";
+                }
+                this.p.direction = "right";
+            } 
+            else if (this.p.vx < 0) { // izquierda
+                if (this.p.landed > 0) {
+                    this.p.sheet = "marioL";
+                } 
+                else {
+                    this.p.sheet = "marioJumpL";
+                }
+                this.p.direction = "left";
+            } 
+            else {
+                if (this.p.direction == "right") {
+                    this.p.sheet = "marioR";
+                } 
+                else {
+                    this.p.sheet = "marioL";
+                }
             }
 
+            if (this.p.vy != 0) {
+                if (this.p.direction == "right") {
+                    this.p.sheet = "marioJumpR";
+                } 
+                else {
+                    this.p.sheet = "marioJumpL";
+                }
+            }
+
+            if (this.p.y > 700) {
+                this.destroy();
+                Q.stageScene("endGame", 1, { label: "You died" });
+                //RestartLevel1();
+            }
+        }
+
+    });
+
+
+    Q.Sprite.extend("Princess", {
+        init: function(p) {
+            this._super(p, {
+                asset: "princess.png"
+            });
+
+            this.add("2d");
+
+            this.on("hit", function(collision) {
+                if (collision.obj.isA("Mario")) {
+                    this.trigger("win");
+                }
+            });
+
+            this.on("win", function() {
+                Q.stageScene("winGame", 1, {
+                    label: "You win!"
+                });
+            });
         }
     });
+
 
 });
