c75ec28b5316137bf4806215b007d8567b386eb2
jricaldi95 <jricaldi@ucm.es>
2019-05-05 18:12:30 +0200

    First commit

---
+++ CarrierAirWing/game.js
@@ -0,0 +1,163 @@
+window.addEventListener("load", function() {
+
+    var Q = window.Q = Quintus({
+            audioSupported: ["ogg", "mp3"]
+        }).include("Sprites, Scenes, Input, 2D, Anim, Touch, UI, TMX, Audio")
+        .enableSound()
+        .setup({
+            width: 720, // width of created canvas
+            height: 480, // height of created canvas
+            scaleToFit: false
+        }).touch().controls();
+
+    //Q.load(["coin.ogg", "music_die.ogg", "music_level_comp lete.ogg", "music_main.ogg"], function() { });
+
+    Q.loadTMX("level.tmx", function() {
+        Q.load("planes.png, planes.json, enemies.png, enemies.json, mainTitle.png", function() {
+            Q.compileSheets("planes.png", "planes.json");
+            Q.compileSheets("enemies.png", "enemies.json");
+            Q.stageScene("mainTitle");
+        });
+
+
+    });
+
+     var StartLevel1 = function() {
+        Q.clearStages();
+        Q.audio.stop();
+        Q.stageScene("level1");
+    };
+
+    Q.scene("mainTitle", function(stage) {
+        var container = stage.insert(new Q.UI.Container({
+            x: Q.width / 2,
+            y: Q.height / 2
+        }));
+
+        //Button
+        var button = container.insert(new Q.UI.Button({
+            x: 0,
+            y: 0,
+            fill: "#CCCCCC",
+            asset: "mainTitle.png"
+        }))
+
+        button.on("click", function() {
+            StartLevel1();
+        });
+        Q.input.on("confirm", this, function() {
+            StartLevel1();
+        });
+
+    });
+
+     Q.scene("level1", function(stage) {
+        Q.stageTMX("level.tmx", stage);
+        var player = stage.insert(new Q.Player());
+
+        //stage.add("viewport").follow(player);
+        //stage.viewport.offsetX = -120;
+        //stage.viewport.offsetY = 95;
+        
+    });
+
+     Q.gravityY = 0;
+
+      Q.animations("anim_player", { 
+        up: {
+            frames: [5,4,3,2,1,0],
+            rate: 1 / 5
+        },
+        stand: {
+            frames: [6],
+            rate: 1 / 5
+        },
+        down: {
+            frames: [7,8,9,10],
+            rate: 1 / 5
+        },
+        fire: {
+            frames: [0,1,2],
+            rate: 1/30,
+            trigger: "fired"
+        }
+    });
+
+    Q.Sprite.extend("Player",{
+
+        init:function(p){
+            this._super(p,{
+                sheet:"tomcat",
+                frame: 6,
+                x: 57,
+                y: 200,
+               /* type:SPRITE_PLAYER,
+                collisionMask:SPRITE_ENEMY,*/
+                speed:200,
+                sprite:"anim_player"
+            });
+
+            this.add("animation,tween");
+            
+             Q.input.on("fire",this,"shoot");
+        },
+        shoot: function() {
+            var p = this.p;
+            this.stage.insert(new Q.Bullet({
+                sheet:"inicial",
+                x: p.x,
+                y: p.y - p.w/2,
+                vy: -200
+            }))
+        
+         },
+    
+        step: function(dt) {
+            /**
+             * Comprar si "LEFT" está siendo pulsado y si el ala derecha del jugador está dentro del canvas.
+             */
+            if (Q.inputs['left'] && (this.p.x - this.p.w/2) > 0) {
+                this.p.vx = -this.p.speed;
+                    
+            } else if (Q.inputs['right'] && (this.p.x + this.p.w/2) < Q.width) {
+                this.p.vx = this.p.speed;
+            } else {
+                this.p.vx = 0;
+            }
+
+            if (Q.inputs['up'] && (this.p.y - this.p.h/2) > 0) {
+                 this.p.vy = -this.p.speed;
+                 //this.p.sheet = "tomcatUp";
+                 this.play("up");
+            } else if (Q.inputs['down'] && (this.p.y + this.p.h/2) < Q.height) {
+                this.p.vy = this.p.speed;
+                //this.p.sheet = "tomcatDown";
+                this.play("down");
+            } else { 
+                this.p.vy = 0;
+                //this.p.sheet = "tomcatDown";
+                this.play("stand");
+
+            }
+
+            if(Q.inputs['space']){
+                this.shoot();
+            }
+ 
+            this.p.x  += this.p.vx * dt;
+            this.p.y  += this.p.vy * dt;
+
+           // Q.input.on("fire", this, "shoot");
+        },
+        shoot: function() {
+            this.stage.insert(new Q.Bullet({
+                x: this.p.x,
+                y: this.p.y - this.p.w/2,
+                vy: -200
+            }))
+        }
+
+    });
+
+
+});
---
+++ CarrierAirWing/practica3.js
@@ -0,0 +1,520 @@
+window.addEventListener("load", function() {
+
+    var Q = window.Q = Quintus({
+            audioSupported: ["ogg", "mp3"]
+        }).include("Sprites, Scenes, Input, 2D, Anim, Touch, UI, TMX, Audio")
+        .enableSound()
+        .setup({
+            width: 720, // width of created canvas
+            height: 480, // height of created canvas
+            scaleToFit: true
+        }).touch().controls();
+
+    //Q.load(["coin.ogg", "music_die.ogg", "music_level_comp lete.ogg", "music_main.ogg"], function() {
+
+    });
+
+    Q.loadTMX("level.tmx", function() {
+        Q.load("mario_small.png, mario_small.json, goomba.png, goomba.json, bloopa.png, bloopa.json, princess.png, mainTitle.png, coin.png, coin.json", function() {
+            Q.compileSheets("mario_small.png", "mario_small.json");
+            Q.compileSheets("goomba.png", "goomba.json");
+            Q.compileSheets("bloopa.png", "bloopa.json");
+            Q.compileSheets("coin.png", "coin.json");
+            Q.stageScene("mainTitle");
+        });
+
+
+    });
+
+    var StartLevel1 = function() {
+        Q.clearStages();
+        Q.audio.stop();
+        Q.stageScene("level1");
+        Q.stageScene("HUD", 1);
+        Q.audio.play("music_main.ogg", {
+            loop: true
+        });
+    };
+
+    var RestartLevel1 = function() {
+        Q.clearStages();
+        Q.stageScene("level1");
+        Q.stageScene("HUD", 1);
+    };
+
+
+    Q.scene("mainTitle", function(stage) {
+        var container = stage.insert(new Q.UI.Container({
+            x: Q.width / 2,
+            y: Q.height / 2
+        }));
+
+        Q.state.set("coins", 0);
+
+        //Button
+        var button = container.insert(new Q.UI.Button({
+            x: 0,
+            y: 0,
+            fill: "#CCCCCC",
+            asset: "mainTitle.png"
+        }))
+
+        button.on("click", function() {
+            StartLevel1();
+        });
+        Q.input.on("confirm", this, function() {
+            StartLevel1();
+        });
+
+    });
+
+    Q.scene("endGame", function(stage) {
+
+        var container = stage.insert(new Q.UI.Container({
+            x: Q.width / 2,
+            y: Q.height / 2,
+            fill: "rgba(0,0,0,0.5)"
+        }));
+
+        var button = container.insert(new Q.UI.Button({
+            x: 0,
+            y: 0,
+            fill: "#CCCCCC",
+            label: "Play Again"
+        }));
+
+        var label = container.insert(new Q.UI.Text({
+            x: 10,
+            y: -10 - button.p.h,
+            label: stage.options.label
+        }));
+
+        Q.state.set("coins", 0);
+
+        button.on("click", function() {
+            StartLevel1();
+        });
+
+        Q.input.on("confirm", this, function() {
+            StartLevel1();
+        });
+
+        container.fit(20);
+
+    });
+
+
+    Q.scene("winGame", function(stage) {
+
+        Q.audio.stop();
+        Q.audio.play("music_level_complete.ogg");
+
+        var container = stage.insert(new Q.UI.Container({
+            x: Q.width / 2,
+            y: Q.height / 2,
+            fill: "rgba(0,0,0,0.5)"
+        }));
+
+        var button = container.insert(new Q.UI.Button({
+            x: 0,
+            y: 0,
+            fill: "#CCCCCC",
+            label: "Play Again"
+        }));
+
+        var label = container.insert(new Q.UI.Text({
+            x: 10,
+            y: -10 - button.p.h,
+            label: stage.options.label
+        }));
+
+        Q.state.set("coins", 0);
+
+        button.on("click", function() {
+            StartLevel1();
+        });
+
+        Q.input.on("confirm", this, function() {
+            StartLevel1();
+        });
+
+        container.fit(20);
+    });
+
+
+    Q.scene("level1", function(stage) {
+        Q.stageTMX("level.tmx", stage);
+        var mario = stage.insert(new Q.Mario());
+
+        stage.add("viewport").follow(mario);
+        stage.viewport.offsetX = -120;
+        stage.viewport.offsetY = 152;
+
+        stage.insert(new Q.Princess({
+            x: 2100,
+            y: 380
+        }));
+
+        stage.insert(new Q.Coin({
+            x: 350,
+            y: 450
+        }));
+        
+        stage.insert(new Q.Coin({
+            x: 650,
+            y: 450
+        }));
+
+        stage.insert(new Q.Coin({
+            x: 1207,
+            y: 375
+        }));
+
+        stage.insert(new Q.Goomba({
+            x: 900,
+            y: 480
+        }));
+
+        stage.insert(new Q.Goomba({
+            x: 1600,
+            y: 380
+        }));
+
+        stage.insert(new Q.Goomba({
+            x: 1700,
+            y: 380
+        }));
+
+        stage.insert(new Q.Bloopa({
+            x: 450,
+            y: 470
+        }));
+
+        stage.insert(new Q.Bloopa({
+            x: 550,
+            y: 470
+        }));
+
+        stage.insert(new Q.Bloopa({
+            x: 1900,
+            y: 380
+        }));
+        
+    });
+
+    Q.scene("HUD", function(stage) {
+        var container = stage.insert(new Q.UI.Container({
+            x: 0,
+            y: 0
+        }));
+        var coins = container.insert(new Q.Coins());
+
+    });
+
+    /* UI */
+
+    Q.UI.Text.extend("Coins", {
+        init: function(p) {
+            this._super({
+                label: "Coins: " + Q.state.p.coins,
+                x: Q.width / 2,
+                y: 0
+            });
+
+            Q.state.on("change.coins", this, "coins");
+        },
+        coins: function(coins) {
+            this.p.label = "Coins: " + coins;
+        }
+    });
+
+
+    //MARIO
+
+    // Mario animations
+    Q.animations("anim_mario", { 
+        run: {
+            frames: [0, 1, 2],
+            rate: 1 / 10
+        },
+        stand: {
+            frames: [0],
+            rate: 1 / 50
+        },
+        jump: {
+            frames: [0],
+            rate: 1 / 50,
+            loop: false
+        }
+    });
+
+    Q.Sprite.extend("Mario", {
+        init: function(p) {
+            this._super(p, {
+                sheet: "marioR",
+                frame: 0,
+                x: 150,
+                y: 380,
+                jumpSpeed: -410,
+                sprite: "anim_mario"
+            });
+
+            this.add("2d, platformerControls, animation, tween");
+
+        },
+        step: function(dt) {
+            if (this.p.vx > 0) { // derecha
+                if (this.p.landed > 0) {
+                    this.p.sheet = "marioR";
+                    this.play("run");
+                } 
+                else {
+                    this.p.sheet = "marioJumpR";
+                    this.play("jump");
+                }
+                this.p.direction = "right";
+            } 
+            else if (this.p.vx < 0) { // izquierda
+                if (this.p.landed > 0) {
+                    this.p.sheet = "marioL";
+                    this.play("run");
+                } 
+                else {
+                    this.p.sheet = "marioJumpL";
+                    this.play("jump");
+                }
+                this.p.direction = "left";
+            } 
+            else {
+                if (this.p.direction == "right") {
+                    this.p.sheet = "marioR";
+                    this.play("stand");
+                } 
+                else {
+                    this.p.sheet = "marioL";
+                    this.play("stand");
+                }
+            }
+
+            if (this.p.vy != 0) {
+                if (this.p.direction == "right") {
+                    this.p.sheet = "marioJumpR";
+                    this.play("jump");
+                } 
+                else {
+                    this.p.sheet = "marioJumpL";
+                    this.play("jump");
+                }
+            }
+
+            if (this.p.y > 700) {
+                this.death();
+                this.destroy();
+                Q.stageScene("endGame", 1, { label: "You died" });
+            }
+
+            if (this.dead == true){
+                this.p.sheet = "marioDie";
+               
+                this.animate({
+                    y: this.p.y - 30,
+                }, 0.3, Q.Easing.Linear, {
+                    callback: function() {
+                       this.destroy();
+                    }
+                });
+            }
+
+
+        },
+        death: function(){
+            this.del("platformerControls");
+            Q.audio.play("music_die.ogg");
+            this.p.vx = 0;
+            this.p.vy = 0;
+            this.dead = true;
+ 
+        }  
+
+    });
+
+
+    // Princess
+
+    Q.Sprite.extend("Princess", {
+        init: function(p) {
+            this._super(p, {
+                asset: "princess.png"
+            });
+
+            this.add("2d");
+
+            this.on("hit", function(collision) {
+                if (collision.obj.isA("Mario")) {
+                    this.trigger("win");
+                }
+            });
+
+            this.on("win", function() {
+                Q.stageScene("winGame", 1, {
+                    label: "You win!"
+                });
+            });
+        }
+    });
+
+
+
+    // Coin
+
+    // Coin animations
+    Q.animations("anim_coin", { 
+        move: {
+            frames: [0, 1, 2],
+            rate: 1 / 6
+        }
+    });
+
+    Q.Sprite.extend("Coin", {
+        init: function(p) {
+            this._super(p, {
+                sheet: "coin",
+                sprite: "anim_coin",
+                z: 0,
+                hit: false,
+                angle: 0,
+                sensor: true,
+                frame: 0
+            });
+
+            this.add("tween, animation");
+
+            this.on("hit", function(collision) {
+                if (collision.obj.isA("Mario") && !this.p.hit) {
+                    this.p.hit = true;
+                    Q.state.inc("coins", 1);
+                    Q.audio.play("coin.ogg");
+                    this.animate({
+                        y: this.p.y - 50,
+                        angle: 0
+                    }, 0.3, Q.Easing.Linear, {
+                        callback: function() {
+                            this.destroy();
+                        }
+                    });
+                }
+            });
+        },
+        step: function(dt){
+            this.play("move");
+        }
+    });
+
+    // Goomba
+
+    // Goomba animations
+    Q.animations("anim_goomba", { 
+        move: {
+            frames: [0, 1],
+            rate: 1 / 6
+        },
+        die: {
+            frames: [2],
+            rate: 1 / 12,
+            loop: false,
+            trigger: "death_event"
+        }
+    });
+
+    Q.Sprite.extend("Goomba",{
+        init: function(p) {
+            this._super(p, { 
+                sheet: 'goomba',
+                sprite: "anim_goomba",
+                frame: 0,
+                vx: 150 });
+          
+            this.add('2d, aiBounce, animation, defaultEnemy');
+
+        }  
+        
+    });
+
+
+    // Bloopa
+
+    // Bloopa animations
+    Q.animations("anim_bloopa", { 
+        move: {
+            frames: [0, 1],
+            rate: 1 / 4
+        },
+        die: {
+            frames: [2],
+            rate: 1 / 10,
+            loop: false,
+            trigger: "death_event"
+        }
+
+    });
+
+    Q.Sprite.extend("Bloopa",{
+        init: function(p) {
+            this._super(p, { 
+                sheet: 'bloopa',
+                sprite: "anim_bloopa",
+                frame: 0});
+          
+            this.add('2d, animation, defaultEnemy');
+
+            this.on("hit", function(collision) {
+                //Si no colisiona con mario
+                if (!collision.obj.isA("Mario")) {
+                    this.p.vy = -270;
+                }
+            });
+
+            // If the enemy gets hit on the top, destroy it
+            // and give the user a "hop"
+            this.on("bump.top",function(collision) {
+
+                if(collision.obj.isA("Mario")) {
+                     this.play("die");
+                     collision.obj.p.vy = -200;
+
+                 }
+            });
+        }
+          
+    });
+
+    Q.component("defaultEnemy", {
+        added: function() {
+            this.entity.play("move");
+
+            this.entity.on("bump.top", function(collision) {
+                if (collision.obj.isA("Mario")) {
+                    this.play("die");
+                     //move enemy
+                    this.p.vy = -300;
+                    //collision.obj.p.vy = -200;
+                }
+            });
+
+            this.entity.on("bump.left, bump.right, bump.bottom", function(collision) {
+                if (collision.obj.isA("Mario")) {
+                    collision.obj.death();
+                    Q.stageScene("endGame",1, { label: "You Died" });
+                    //collision.obj.destroy();
+                }
+            });
+
+            this.entity.on("death_event", this, function() {
+                this.entity.p.vx = 0;
+                this.entity.destroy();
+            });
+        }
+    });
+
+
+});
